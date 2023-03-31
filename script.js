c68f769ef81a7358784fcc42252e0578e98ca543
TheCodingZone <anuragtiwari14447@gamil.com>
2023-03-31 20:57:37 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var list=document.getElementById('items');
+list.style.color='red';
+list.style.fontFamily='bold';
+
+var listThird=document.querySelector('.list-group-item:nth-child(3)');
+listThird.style.backgroundColor='#90EE90';
